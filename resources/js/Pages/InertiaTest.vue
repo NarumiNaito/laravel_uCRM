922689b930913fc7cb7b9fcd2382fc198f804670
Naito <naito01030103@icloud.com>
2023-11-30 16:28:36 +0900

    sec02_inertia_vue

---
+++ resources/js/Pages/InertiaTest.vue
@@ -0,0 +1,31 @@
+<script setup>
+import { Link } from "@inertiajs/inertia-vue3";
+import { ref } from "vue";
+
+const newTitle = ref("");
+const newContent = ref("");
+</script>
+
+<template>
+    inertiaテスト
+    <a href="/">aタグ経由</a><br />
+    <Link href="/">Link経由</Link><br />
+    <Link :href="route('inertia.index')">名前付きルート確認</Link><br />
+    <Link :href="route('inertia.show', { id: 80 })"
+        >ルートパラメーターテスト
+    </Link>
+
+    <div class="mb-8"></div>
+    <input type="text" name="newTitle" v-model="newTitle" /><br />
+    <input type="text" name="newContent" v-model="newContent" /><br />
+    <Link
+        as="button"
+        method="post"
+        :href="route('inertia.store')"
+        :date="{
+            title: newTitle,
+            content: newContent,
+        }"
+        >DB保存</Link
+    >
+</template>
---
+++ resources/js/Pages/__VLS_template.1.js
@@ -0,0 +1,100 @@
+import { Link } from "@inertiajs/inertia-vue3";
+import { __VLS_internalComponent, __VLS_componentsOption, __VLS_name } from "./InertiaTest.vue";
+
+function __VLS_template() {
+let __VLS_ctx!: InstanceType<__VLS_PickNotAny<typeof __VLS_internalComponent, new () => {}>> & {};
+/* Components */
+let __VLS_otherComponents!: NonNullable<typeof __VLS_internalComponent extends { components: infer C; } ? C : {}> & typeof __VLS_componentsOption;
+let __VLS_own!: __VLS_SelfComponent<typeof __VLS_name, typeof __VLS_internalComponent & (new () => { $slots: typeof __VLS_slots; })>;
+let __VLS_localComponents!: typeof __VLS_otherComponents & Omit<typeof __VLS_own, keyof typeof __VLS_otherComponents>;
+let __VLS_components!: typeof __VLS_localComponents & __VLS_GlobalComponents & typeof __VLS_ctx;
+/* Style Scoped */
+type __VLS_StyleScopedClasses = {};
+let __VLS_styleScopedClasses!: __VLS_StyleScopedClasses | keyof __VLS_StyleScopedClasses | (keyof __VLS_StyleScopedClasses)[];
+/* CSS variable injection */
+/* CSS variable injection end */
+let __VLS_resolvedLocalAndGlobalComponents!: {} &
+__VLS_WithComponent<'Link', typeof __VLS_localComponents, "Link", "Link", "Link">;
+({} as __VLS_IntrinsicElements).a; ({} as __VLS_IntrinsicElements).a;
+({} as __VLS_IntrinsicElements).br; ({} as __VLS_IntrinsicElements).br; ({} as __VLS_IntrinsicElements).br;
+__VLS_components.Link; __VLS_components.Link; __VLS_components.Link; __VLS_components.Link; __VLS_components.Link; __VLS_components.Link;
+// @ts-ignore
+[Link, Link, Link, Link, Link, Link,];
+{
+const __VLS_0 = ({} as __VLS_IntrinsicElements)["a"];
+const __VLS_1 = __VLS_elementAsFunctionalComponent(__VLS_0);
+({} as __VLS_IntrinsicElements).a;
+({} as __VLS_IntrinsicElements).a;
+const __VLS_2 = __VLS_1({ ...{}, href: ("/"), }, ...__VLS_functionalComponentArgsRest(__VLS_1));
+({} as (props: __VLS_FunctionalComponentProps<typeof __VLS_0, typeof __VLS_2> & Record<string, unknown>) => void)({ ...{}, href: ("/"), });
+const __VLS_3 = __VLS_pickFunctionalComponentCtx(__VLS_0, __VLS_2)!;
+let __VLS_4!: __VLS_NormalizeEmits<typeof __VLS_3.emit>;
+(__VLS_3.slots!).default;
+}
+{
+const __VLS_5 = ({} as __VLS_IntrinsicElements)["br"];
+const __VLS_6 = __VLS_elementAsFunctionalComponent(__VLS_5);
+({} as __VLS_IntrinsicElements).br;
+const __VLS_7 = __VLS_6({ ...{}, }, ...__VLS_functionalComponentArgsRest(__VLS_6));
+({} as (props: __VLS_FunctionalComponentProps<typeof __VLS_5, typeof __VLS_7> & Record<string, unknown>) => void)({ ...{}, });
+const __VLS_8 = __VLS_pickFunctionalComponentCtx(__VLS_5, __VLS_7)!;
+let __VLS_9!: __VLS_NormalizeEmits<typeof __VLS_8.emit>;
+}
+{
+let __VLS_10!: 'Link' extends keyof typeof __VLS_ctx ? typeof __VLS_ctx.Link : (typeof __VLS_resolvedLocalAndGlobalComponents)['Link'];
+const __VLS_11 = __VLS_asFunctionalComponent(__VLS_10, new __VLS_10({ ...{}, href: ("/"), }));
+({} as { Link: typeof __VLS_10; }).Link;
+({} as { Link: typeof __VLS_10; }).Link;
+const __VLS_12 = __VLS_11({ ...{}, href: ("/"), }, ...__VLS_functionalComponentArgsRest(__VLS_11));
+({} as (props: __VLS_FunctionalComponentProps<typeof __VLS_10, typeof __VLS_12> & Record<string, unknown>) => void)({ ...{}, href: ("/"), });
+const __VLS_13 = __VLS_pickFunctionalComponentCtx(__VLS_10, __VLS_12)!;
+let __VLS_14!: __VLS_NormalizeEmits<typeof __VLS_13.emit>;
+(__VLS_13.slots!).default;
+}
+{
+const __VLS_15 = ({} as __VLS_IntrinsicElements)["br"];
+const __VLS_16 = __VLS_elementAsFunctionalComponent(__VLS_15);
+({} as __VLS_IntrinsicElements).br;
+const __VLS_17 = __VLS_16({ ...{}, }, ...__VLS_functionalComponentArgsRest(__VLS_16));
+({} as (props: __VLS_FunctionalComponentProps<typeof __VLS_15, typeof __VLS_17> & Record<string, unknown>) => void)({ ...{}, });
+const __VLS_18 = __VLS_pickFunctionalComponentCtx(__VLS_15, __VLS_17)!;
+let __VLS_19!: __VLS_NormalizeEmits<typeof __VLS_18.emit>;
+}
+{
+let __VLS_20!: 'Link' extends keyof typeof __VLS_ctx ? typeof __VLS_ctx.Link : (typeof __VLS_resolvedLocalAndGlobalComponents)['Link'];
+const __VLS_21 = __VLS_asFunctionalComponent(__VLS_20, new __VLS_20({ ...{}, href: ((__VLS_ctx.route('inertia.index'))), }));
+({} as { Link: typeof __VLS_20; }).Link;
+({} as { Link: typeof __VLS_20; }).Link;
+const __VLS_22 = __VLS_21({ ...{}, href: ((__VLS_ctx.route('inertia.index'))), }, ...__VLS_functionalComponentArgsRest(__VLS_21));
+({} as (props: __VLS_FunctionalComponentProps<typeof __VLS_20, typeof __VLS_22> & Record<string, unknown>) => void)({ ...{}, href: ((__VLS_ctx.route('inertia.index'))), });
+const __VLS_23 = __VLS_pickFunctionalComponentCtx(__VLS_20, __VLS_22)!;
+let __VLS_24!: __VLS_NormalizeEmits<typeof __VLS_23.emit>;
+(__VLS_23.slots!).default;
+}
+{
+const __VLS_25 = ({} as __VLS_IntrinsicElements)["br"];
+const __VLS_26 = __VLS_elementAsFunctionalComponent(__VLS_25);
+({} as __VLS_IntrinsicElements).br;
+const __VLS_27 = __VLS_26({ ...{}, }, ...__VLS_functionalComponentArgsRest(__VLS_26));
+({} as (props: __VLS_FunctionalComponentProps<typeof __VLS_25, typeof __VLS_27> & Record<string, unknown>) => void)({ ...{}, });
+const __VLS_28 = __VLS_pickFunctionalComponentCtx(__VLS_25, __VLS_27)!;
+let __VLS_29!: __VLS_NormalizeEmits<typeof __VLS_28.emit>;
+}
+{
+let __VLS_30!: 'Link' extends keyof typeof __VLS_ctx ? typeof __VLS_ctx.Link : (typeof __VLS_resolvedLocalAndGlobalComponents)['Link'];
+const __VLS_31 = __VLS_asFunctionalComponent(__VLS_30, new __VLS_30({ ...{}, href: ((__VLS_ctx.route('inertia.show', { id: 1 }))), }));
+({} as { Link: typeof __VLS_30; }).Link;
+({} as { Link: typeof __VLS_30; }).Link;
+const __VLS_32 = __VLS_31({ ...{}, href: ((__VLS_ctx.route('inertia.show', { id: 1 }))), }, ...__VLS_functionalComponentArgsRest(__VLS_31));
+({} as (props: __VLS_FunctionalComponentProps<typeof __VLS_30, typeof __VLS_32> & Record<string, unknown>) => void)({ ...{}, href: ((__VLS_ctx.route('inertia.show', { id: 1 }))), });
+const __VLS_33 = __VLS_pickFunctionalComponentCtx(__VLS_30, __VLS_32)!;
+let __VLS_34!: __VLS_NormalizeEmits<typeof __VLS_33.emit>;
+(__VLS_33.slots!).default;
+}
+if (typeof __VLS_styleScopedClasses === 'object' && !Array.isArray(__VLS_styleScopedClasses)) {
+}
+var __VLS_slots!: {};
+// @ts-ignore
+[route, route, route, route, route, route,];
+return __VLS_slots;
+}
